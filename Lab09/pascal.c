9293bb8592533265a3a7c961c11234dd3c4abc0d
ronghuang97 <huang.rong@northeastern.edu>
2024-04-09 13:23:51 -0400

    CS5008 Lab09 Files for Submission

--- Lab09/pascal.c
+++ Lab09/pascal.c
@@ -1,5 +1,5 @@
-// Your Name Goes Here
-// The Date Goes Here
+// Rong Huang
+// 04/08/2024
 // Lab09
 //
 // Compile with: gcc -Wall pascal.c -o pascal
@@ -18,39 +18,63 @@
 //
 // coincidentally, this problem is the same as one of the problems
 // on LeetCode: https://leetcode.com/problems/pascals-triangle-ii/
-int* getRow(int rowIndex, int* returnSize){
-   // TODO: Implement me!!
+int *getRow(int rowIndex, int *returnSize)
+{
+    // TODO: Implement me!!
+    if (returnSize == NULL) {return NULL;}
+    *returnSize = rowIndex + 1; // The size of the row is rowIndex + 1
+    int *row = (int *)malloc(*returnSize * sizeof(int));
+    if (row == NULL) {
+        *returnSize = 0;
+        return NULL;
+    }
 
-   return NULL;
+    row[0] = 1; // The first element of each row is always 1
+
+    // Calculate values for the row using memoization
+    for (int i = 1; i <= rowIndex; i++)
+    {
+        row[i] = 1; // The last element of each row is also 1
+        for (int j = i - 1; j > 0; j--)
+        {
+            // Update the value of current element by summing
+            // the two directly above it, so we update it from right to left
+            row[j] = row[j] + row[j - 1];
+        }   
+    }
+    return row; // Return the pointer to the row
 }
 
+int main(int argc, char *argv[])
+{
+    // Make sure that there is one argument
+    if (argc != 2)
+    {
+        printf("One argument expected: ./pascal 42\n");
+        return 1;
+    }
 
-int main(int argc, char* argv[]) {
-   // Make sure that there is one argument
-   if (argc != 2) {
-      printf("One argument expected: ./pascal 42\n");
-      return 1;
-   }
+    // Convert the argument of the program into an integer
+    // and make sure that it is between 0 and LARGEST_TRIANGLE - 1
+    const int row = atoi(argv[1]);
+    if (row < 0 || row > LARGEST_TRIANGLE - 1)
+    {
+        printf("row must between 0 and %d (inclusive)\n", LARGEST_TRIANGLE - 1);
+        return 1;
+    }
 
-   // Convert the argument of the program into an integer
-   // and make sure that it is between 0 and LARGEST_TRIANGLE - 1
-   const int row = atoi(argv[1]);
-   if (row < 0 || row > LARGEST_TRIANGLE - 1) {
-      printf("row must between 0 and %d (inclusive)\n", LARGEST_TRIANGLE - 1);
-      return 1;
-   }
+    int size = 0;
+    int *row_values = getRow(row, &size);
+    int i;
 
-   int size = 0;
-   int* row_values = getRow(row, &size);
-   int i;
+    printf("Row %d of Pascal's Triangle\n", row);
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", row_values[i]);
+    }
+    printf("\n");
 
-   printf("Row %d of Pascal's Triangle\n", row);
-   for(i = 0 ; i < size ; i++) {
-      printf("%d ", row_values[i]);
-   }
-   printf("\n");
-
-   // free the memory that was returned from function
-   free(row_values);
-   return 0;
+    // free the memory that was returned from function
+    free(row_values);
+    return 0;
 }
